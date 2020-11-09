270591d33fe93856a06c3b2f3b016e08de4b1363
Shrishailya Sutar <shreesutar96@gmail.com>
2020-11-09 11:23:09 +0530

    1

--- app/src/main/java/com/example/myfinalproject/MainActivity.java
+++ app/src/main/java/com/example/myfinalproject/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.myfinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     TextView gotoreg,gotofpass;
     Button btnsignin;
     EditText etmail,etpass;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         btnsignin=findViewById(R.id.button);
         etmail=findViewById(R.id.editTextTextEmailAddress);
         etpass=findViewById(R.id.editTextTextPassword);
-
+        mAuth=FirebaseAuth.getInstance();
         gotoreg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,7 +50,42 @@ public class MainActivity extends AppCompatActivity {
         btnsignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,category.class));
+                userlogin();
+            }
+        });
+    }
+
+    private void userlogin() {
+        String email=etmail.getText().toString().trim();
+        String pass=etpass.getText().toString().trim();
+        if(email.isEmpty())
+        {
+            etmail.setError("Please enter email");
+            etmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            etmail.setError("Please enter valid email");
+            etmail.requestFocus();
+            return;
+        }
+        if(pass.isEmpty()) {
+            etpass.setError("Please enter password");
+            etpass.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    startActivity(new Intent(MainActivity.this,category.class));
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Failed to login! Please, check your login credentials",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
